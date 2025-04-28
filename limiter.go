a7d7d72fb19e139f0de43f0005fdadd38caf7c9b
Reyhane <161202488+reihane-rangamiz@users.noreply.github.com>
2025-04-28 22:36:44 +0330

    Update limiter.go

--- limiter.go
+++ limiter.go
@@ -1,1 +1,12 @@
 package main
+
+
+import (
+    "time"
+)
+
+type RateLimiter struct {
+    rate       int           // tokens per interval
+    interval   time.Duration // refill interval
+    tokens     chan struct{} // token bucket
+}
