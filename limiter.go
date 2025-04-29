620f34443e4b9ac616436121c763700b7e13c2ce
Reyhane Rangamiz <ri.rangamiz@gmail.com>
2025-04-29 17:14:12 +0330

    fixed window limiter + test

--- limiter.go
+++ limiter.go
@@ -1,12 +1,56 @@
 package ratelimiter
 
-
 import (
-    "time"
+	"sync"
+	"time"
 )
 
-type RateLimiter struct {
-    rate       int           // tokens per interval
-    interval   time.Duration // refill interval
-    tokens     chan struct{} // token bucket
+type RateLimiter interface {
+	Allow() bool
+}
+
+type FixedWindowRateLimiter struct {
+	Rate     int           // maximum request
+	Interval time.Duration // the determined duration
+	Mu       sync.Mutex
+	Requests []time.Time // recent request tracks
+}
+
+func NewFixedWindowRateLimiter(rate int, interval time.Duration) *FixedWindowRateLimiter {
+	return &FixedWindowRateLimiter{
+		Rate:     rate,
+		Interval: interval,
+		Requests: make([]time.Time, 0),
+	}
+}
+
+// Allow() Tracks timestamps of requests.
+// On every Allow():
+// Removes old timestamps (outside the current window).
+// Checks how many remain.(based on the rate)
+// If less than rate, allow and add new timestamp.
+// Otherwise, deny.
+// Uses sync.Mutex to protect data in concurrent access.
+func (f *FixedWindowRateLimiter) Allow() bool {
+	f.Mu.Lock() // to ensure safe concurrent access to the request
+	defer f.Mu.Unlock()
+
+	now := time.Now()
+	limitTime := now.Add(-f.Interval) // check the start of the duration which we should consider(calc limit window)
+
+	i := 0
+	for _, t := range f.Requests {
+		if t.After(limitTime) {
+			break
+		}
+		i++
+	}
+	f.Requests = f.Requests[i:] // remove the checked request time and keep the rest to examine
+
+	if len(f.Requests) < f.Rate {
+		f.Requests = append(f.Requests, now) // we should add the new request to the request list and keep the track of them
+		return true
+	}
+
+	return false
 }
--- limiter_test.go
+++ limiter_test.go
@@ -1,1 +1,30 @@
 package ratelimiter
+
+import (
+	"testing"
+	"time"
+)
+
+func TestFixedWindowRateLimiter(t *testing.T) {
+	// sec:= time.Duration(time.Second)*10
+	newLimiter := NewFixedWindowRateLimiter(3, time.Second)
+
+	for i := 0; i < 3; i++ {
+		if !newLimiter.Allow() {
+			t.Errorf("request must have been allowed since we didnt hit the rate")
+		}
+	}
+
+	// this is more than rate, If it returns true, it means the limiter failed to block the extra request (→ That's a bug). hence we wait a second and then check allow 
+	if newLimiter.Allow() { //negative test 
+		// Bad! We should NOT be allowed to send the 4th request in the same window
+		t.Errorf("request should have been blocked since we reached the rate limit")
+	}
+
+	time.Sleep(time.Second)
+
+	if !newLimiter.Allow() {
+		// now we reset the window and first 3 requests supposed to be allowed 
+		t.Errorf("Request after window reset should be allowed")
+	}
+}
